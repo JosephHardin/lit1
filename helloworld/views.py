2504d5c88932cba0228d5c2908284f1c8dceec19
JosephHardin <joe.hardin369@gmail.com>
2023-08-22 12:50:13 -0400

    Added login page(incomplete)

--- helloworld/views.py
+++ helloworld/views.py
@@ -6,3 +6,7 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'home.html', context=None)
+
+class LoginView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'login.html', context=None)
