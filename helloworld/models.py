364f3a4f32c1c13383b01fba5a0f3b274686f889
JosephHardin <joe.hardin369@gmail.com>
2023-08-22 15:38:00 -0400

    created user member and form

--- helloworld/models.py
+++ helloworld/models.py
@@ -1,3 +1,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+class User(models.Model):
+    username = models.CharField(max_length=255, primary_key=True)
+    email = models.EmailField()
+    password = models.CharField(max_length=25)
+    def __str__(self):
+        return self.username
+
+
