fa94e319dc60f98538373349c2fde6d7e57f4ac5
JosephHardin <joe.hardin369@gmail.com>
2023-08-30 16:00:10 -0400

    starting the transfer from sqlite to aws hosted postgres
    cross your fingers

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ebdjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'litdb',
+        'USER': 'postgres',
+        'PASSWORD': 'college4',
+        'HOST': 'database-1.cqmyzcbz1ltq.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
